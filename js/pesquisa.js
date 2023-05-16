0a5a20389748035741299818e2470d4d9b77bbda
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-05-16 15:55:42 -0300

    feat: implementando a lógica de filtragem

---
+++ js/pesquisa.js
@@ -0,0 +1,22 @@
+var campoPesquisa = document.querySelector("#filtrar-tabela");
+
+campoPesquisa.addEventListener("input", function () {
+  var pacientes = document.querySelectorAll(".paciente");
+  if (this.value.length > 0) {
+    for (var i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      var nomePesquisa = paciente.querySelector(".info-nome");
+      var nome = nomePesquisa.textContent;
+      if (nome != this.value) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  } else {
+    for (var i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      paciente.classList.remove("invisivel");
+    }
+  }
+});
