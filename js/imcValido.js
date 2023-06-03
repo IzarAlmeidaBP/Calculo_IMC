9ef32392ee0d67b3aa39bdacb961386b23799d34
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-06-03 19:43:16 -0300

    feat: adicionando uma função

--- js/imcValido.js
+++ js/imcValido.js
@@ -53,3 +53,18 @@ function calculaImc(peso, altura) {
   imc = peso / (altura * altura);
   return imc.toFixed(2);
 }
+function classificarPaciente(imc) {
+  if (imc < 18.5) {
+    return "Abaixo do peso";
+  } else if (imc >= 18.5 && imc < 25) {
+    return "Peso normal";
+  } else if (imc >= 25 && imc < 30) {
+    return "Sobrepeso";
+  } else if (imc >= 30 && imc < 35) {
+    return "Obesidade grau 1";
+  } else if (imc >= 35 && imc < 40) {
+    return "Obesidade grau 2";
+  } else {
+    return "Obesidade grau 3";
+  }
+}
